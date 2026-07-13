355c972dbb2f0c06599c3b4f9a93c747da38fdc7
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-07-13 15:32:23 +0530

    usestate toggle

--- react-js-tutorial/src/Usestatefn.tsx
+++ react-js-tutorial/src/Usestatefn.tsx
@@ -1,15 +1,26 @@
 "use client";
 import { useState } from "react";
 
-function Usestatefn(){
-    const [count, setcount] = useState(0);
-    return <div>
+function Usestatefn() {
+  const [count, setcount] = useState(0);
+  const [display, setDispaly] = useState(true);
+  return (
+    <div>
+      {/* counter */}
+      <div>
         <p>Count : {count}</p>
 
-        <button onClick={()=>setcount(count+1)}>increment the count</button>
+        <button onClick={() => setcount(count + 1)}>increment the count</button>
 
-        <button onClick={()=>setcount(count-1)}>Decrement the count</button>
+        <button onClick={() => setcount(count - 1)}>Decrement the count</button>
+      </div>
+      {/* Toggle Button */}
+      <div>
+        {display ? <p>Hello</p> : ""}
+        <button onClick={() => setDispaly(!display)}>click here</button>
+      </div>
     </div>
+  );
 }
 
-export default Usestatefn
+export default Usestatefn;
