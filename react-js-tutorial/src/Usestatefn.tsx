98567d594e01e58f7e27254c2953af2039409a29
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2026-07-13 15:50:06 +0530

    use state form

--- react-js-tutorial/src/Usestatefn.tsx
+++ react-js-tutorial/src/Usestatefn.tsx
@@ -4,8 +4,17 @@ import { useState } from "react";
 function Usestatefn() {
   const [count, setcount] = useState(0);
   const [display, setDispaly] = useState(true);
+
+  const [name, setName] = useState("");
+  const [submitname, setSubmitname] = useState("");
+
+  const displaydata = (e) => {
+    e.preventDefault();
+    setSubmitname(name);
+  };
   return (
     <div>
+        
       {/* counter */}
       <div>
         <p>Count : {count}</p>
@@ -14,11 +23,27 @@ function Usestatefn() {
 
         <button onClick={() => setcount(count - 1)}>Decrement the count</button>
       </div>
+
       {/* Toggle Button */}
       <div>
         {display ? <p>Hello</p> : ""}
         <button onClick={() => setDispaly(!display)}>click here</button>
       </div>
+
+      {/* Input Field */}
+      <div>
+        <form onSubmit={displaydata}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">submit</button>
+        </form>
+        <br />
+        <p>Name: {submitname} </p>
+      </div>
+
     </div>
   );
 }
